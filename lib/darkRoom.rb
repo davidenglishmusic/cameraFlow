0e0c1e43e8519ae857a8eaa0c277dd4765f595ae
David English <david@operaonfilm.com>
2015-02-01 21:45:01 -0800

    Partially refactor DarkRoom class and added an initial spec

--- lib/darkRoom.rb
+++ lib/darkRoom.rb
@@ -20,16 +20,18 @@ class DarkRoom
   def process_image(file_path, filename, extension, coordinates, count)
     image = ImageList.new(file_path)
     scaled_image = image.scale(@enlargement_factor)
-    adjusted_image = get_subpixel_crop(scaled_image, 0 + coordinates[0], 0 + coordinates[1], @HD_width + coordinates[0], @HD_height + coordinates[1])
+    distortion_settings = create_distortion_settings(0 + coordinates[0], 0 + coordinates[1], @HD_width + coordinates[0], @HD_height + coordinates[1])
+    p distortion_settings
+    adjusted_image = distort_image(scaled_image, distortion_settings[:distance_to_centre_x], distortion_settings[:distance_to_centre_y], distortion_settings[:final_width], distortion_settings[:final_height])
     adjusted_image.write "output/#{filename + count.to_s + '.png'}"
     adjusted_image.destroy!
   end
 
-  def get_subpixel_crop(image, left, top, right, bottom)
-    final_width = @HD_width
-    final_height = @HD_height
-    distance_to_centre_x = distance_to_centre(left, right)
-    distance_to_centre_y = distance_to_centre(top, bottom)
+  def create_distortion_settings(left, top, right, bottom)
+    {:final_width => @HD_width, :final_height => @HD_height, :distance_to_centre_x => distance_to_centre(left, right), :distance_to_centre_y => distance_to_centre(top, bottom)}
+  end
+
+  def distort_image(image, distance_to_centre_x, distance_to_centre_y, final_width, final_height)
     image.distort(Magick::ScaleRotateTranslateDistortion, [distance_to_centre_x, distance_to_centre_y, SCALE_X, SCALE_Y, 0.0, POINT_FIVE * final_width, POINT_FIVE * final_height]) do |i|
       i.define('distort:viewport', "#{final_width}x#{final_height}+0+0")
     end
--- spec/darkRoom_spec.rb
+++ spec/darkRoom_spec.rb
@@ -1,1 +1,15 @@
 require_relative '../lib/darkRoom'
+
+describe DarkRoom do
+
+  before :all do
+    @test_object_001 = DarkRoom.new(1.052083333, [1920,1080])
+  end
+
+  describe 'distance_to_centre' do
+    it 'returns the distance to the center from the sides' do
+      expect(@test_object_001.distance_to_centre(0, 1920)).to eq(960)
+    end
+  end
+
+end
