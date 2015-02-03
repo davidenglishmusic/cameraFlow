6a2867554b3c3f8bb6ce709bbaa5c5e174ac788b
David English <david@operaonfilm.com>
2015-02-03 08:27:47 -0800

    Added a new version of the keyframe generator

--- lib/keyframeGen.rb
+++ lib/keyframeGen.rb
@@ -1,6 +1,8 @@
 class KeyframeGen
+  BUFFER_ZONE = 3
+
   attr_accessor :number_of_keyframes_to_generate
-  attr_accessor :total_frame_of_clip
+  attr_accessor :total_frames_of_clip
   attr_accessor :proximity_minimum
 
   def initialize(number_of_keyframes_to_generate, total_frames_of_clip, proximity_minimum)
@@ -9,23 +11,40 @@ class KeyframeGen
     @proximity_minimum = proximity_minimum
   end
 
+  def possible_to_calculate?
+    if @total_frames_of_clip / @proximity_minimum <= @number_of_keyframes_to_generate
+      true
+    else
+      false
+    end
+  end
+
+  def easy_to_calculate?
+    if @number_of_keyframes_to_generate < (@total_frames_of_clip / @proximity_minimum) - BUFFER_ZONE
+      true
+    else
+      false
+    end
+  end
+
   def generate_keyframes
     arr = []
     (0..@number_of_keyframes_to_generate).each do
-      uniqueness_unknown = true
-      while uniqueness_unknown
-        new_frame = generate_frame
-        if unique?(new_frame, arr) && !too_close?(new_frame, arr)
-          arr.push(new_frame)
-          uniqueness_unknown = false
-        end
-      end
+      arr.push(generate_frame(arr))
     end
     arr.sort
   end
 
-  def generate_frame
-    rand(0..@total_frames_of_clip)
+  def generate_frame(arr)
+    uniqueness_unknown = true
+    new_frame = 0
+    while uniqueness_unknown
+      new_frame = rand(0..@total_frames_of_clip)
+      if unique?(new_frame, arr) && !too_close?(new_frame, arr)
+        uniqueness_unknown = false
+      end
+    end
+    new_frame
   end
 
   def unique?(new_frame, arr)
